f1660f6966aa1a8be82544e9f64ebc6549001b07
James Plummer <jamesp2001@live.co.uk>
2025-08-03 22:07:20 +0200

    add nix flake with tooling

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "AVR-HAL Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      fenix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            fenix.packages.${system}.complete.toolchain
+            pkgs.pkgsCross.avr.buildPackages.gcc
+            pkgs.pkg-config
+            pkgs.avrdude
+            pkgs.ravedude
+          ];
+        };
+      }
+    );
+}
